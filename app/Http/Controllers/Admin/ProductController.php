1cfe8df6f86d2a19609914c64afd1e9ad37612ec
Fandi Akhmad <fanjavaid@gmail.com>
2015-09-13 15:16:34 +0700

    Bug fix and performance improvements

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -11,6 +11,7 @@ use App\Category;
 use App\Product;
 use Validator;
 use Session;
+use File;
 
 class ProductController extends Controller
 {
@@ -135,13 +136,52 @@ class ProductController extends Controller
     public function edit($id)
     {
         //
+        
         $product = Product::findOrFail($id);
         $categories = Category::lists('category_name', 'id');
         $attributes = Attribute::all();
+        
+        $attrElements = array();
+        foreach ($attributes as $attr) {
+            $element = array();
+            if ($attr->type == 'text') {
+                $element = array(
+                    'id' => $attr->id,
+                    'name' => $attr->attribute_name,
+                    'type' => 'text',
+                    'element' => "<input type='text' name='attributes[". $attr->id ."][value]' value='' class='form-control' />"
+                );
+            } else {
+                $element = array(
+                    'id' => $attr->id,
+                    'name' => $attr->attribute_name,
+                    'type' => 'editor',
+                    'element' => "<textarea name='attributes[". $attr->id ."][value]' class='form-control'></textarea>"
+                );
+            }
 
+            array_push($attrElements, $element);
+        }
+
+        foreach ($attrElements as &$attrEl) {
+            foreach ($product->attributes as $pAttr) {
+                if ($pAttr->id == $attrEl['id']) {
+                    // echo $pAttr->id  . " == " . $attrEl['id'] . " ? " . ($pAttr->id == $attrEl['id']);
+                    // echo "<br/>";
+                    if ($attrEl['type'] == 'text') {
+                        $attrEl['element'] = "<input type='text' class='form-control' name='attributes[". $attrEl['id'] ."][value]' value='" . $pAttr->pivot->value . "' />";
+                    } else {
+                        $attrEl['element'] = "<textarea name='attributes[". $attrEl['id'] ."][value]' class='form-control'>" . $pAttr->pivot->value . "</textarea>";
+                    }
+                }
+            }
+        }
+
+        // dd($product->attributes);
+        // dd($attrElements);
         return view('product.edit', $this->data)
                     ->withProduct($product)
-                    ->withAttributes($attributes)
+                    ->with('attrElements', $attrElements)
                     ->withCategories($categories);
     }
 
@@ -154,7 +194,54 @@ class ProductController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $product = Product::findOrFail($id);
+
+        // Validation
+        $rules = [
+            'code' => 'required',
+            'product_name' => 'required',
+            'sku' => 'required|numeric',
+            'price' => 'required|numeric',
+            'discount' => 'required|numeric',
+            'categories' => 'required',
+            'featured_image' => 'mimes:png,jpg,jpeg,bmp',
+        ];
+
+        $validator = Validator::make($request->all(), $rules);
+
+        if ($validator->fails()) {
+            return redirect()->back()->withErrors($validator);
+        } else {
+            // Get Input
+            $input = array(
+                'code' => $request->input('code'),
+                'product_name' => $request->input('product_name'),
+                'sku' => $request->input('sku'),
+                'price' => $request->input('price'),
+                'discount' => $request->input('discount'),
+                'description' => $request->input('description'),
+                'featured_image' => ($request->file('featured_image') != null) ? $request->file('featured_image')->getClientOriginalName() : $product->featured_image,
+            );
+            $categories = $request->input('categories');
+            $attributes = $request->input('attributes');
+
+            // Save and Get Inserted ID
+            // $product_id = Product::create($input)->id;
+            $product->fill($input)->save();
+
+            if ($request->file('featured_image') != null) {
+                // Upload featured image
+                $imageName  = $request->file('featured_image')->getClientOriginalName();
+                $request->file('featured_image')->move(base_path() . '/public/images/product/', $imageName);
+            }
+
+            // Save to Join Table
+            Product::find($id)->categories()->sync($categories);
+            Product::find($id)->attributes()->sync($attributes);
+
+            Session::flash('flash_message', 'Product successfully updated!');
+            return redirect()->back();
+        }
     }
 
     /**
@@ -166,5 +253,17 @@ class ProductController extends Controller
     public function destroy($id)
     {
         //
+        $product = Product::findOrFail($id);
+        $product->delete();
+
+        // $this->removeImage($product->featured_image);
+        Session::flash('flash_message', 'Product <strong>' . $product->product_name . '</strong> successfully deleted!');
+        return redirect()->back();
+    }
+
+    public function removeImage($imageName) {
+        File::Delete(base_path() . '/public/images/product/' . $imageName);
+
+        return redirect()->back();
     }
 }
